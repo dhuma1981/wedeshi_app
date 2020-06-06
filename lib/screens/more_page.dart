f93a575c2eae346669c1800eb787d433aca852a4
Dhrumil Shah <dhuma1981@gmail.com>
2020-06-06 16:10:59 +0530

    more page UI done

--- lib/screens/more_page.dart
+++ lib/screens/more_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:launch_review/launch_review.dart';
 
 class MorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("More Page"),
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.help_outline),
+          title: Text("About Us"),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.info_outline),
+          title: Text("Disclaimer"),
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.feedback),
+          title: Text("Feedback"),
+        ),
+        Divider(),
+        ListTile(
+          onTap: () {
+            LaunchReview.launch();
+          },
+          leading: Icon(Icons.rate_review),
+          title: Text("Rate Us"),
+        ),
+        Divider(),
+      ],
     );
   }
 }
